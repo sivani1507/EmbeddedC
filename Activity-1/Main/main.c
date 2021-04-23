817008c4cdd83db1ff800fde6cf693cc13fe3454
sivani1507 <80464851+sivani1507@users.noreply.github.com>
2021-04-23 20:57:44 +0530

    Add files via upload

--- Activity-1/Main/main.c
+++ Activity-1/Main/main.c
@@ -1,1 +1,31 @@
+/*
+ */
 
+#include <avr/io.h>
+#include<util/delay.h>
+int main(void)
+{
+
+   DDRB|=(1<<PB0);
+    // Insert code
+  DDRD&=~(1<<PD2);
+  PORTD|=(1<<PD2);
+  DDRD&=~(1<<PD1);
+  PORTD|=(1<<PD1);
+
+    while(1)
+    {
+        if((!(PIND&(1<<PD2))) & (!(PIND&(1<<PD1))))
+        {
+
+        PORTB|=(1<<PB0);
+        _delay_ms(200);
+        }
+        else{
+        PORTB&=~(1<<PB0);
+        _delay_ms(200);
+    }
+    }
+
+    return 0;
+}
