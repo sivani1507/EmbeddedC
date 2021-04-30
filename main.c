af3f3d0caed874e7b58874a42d42886fc184a0e7
sivani1507 <80464851+sivani1507@users.noreply.github.com>
2021-04-30 15:57:25 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -13,12 +13,13 @@
 #include "activity3.h"
 #include "activity4.h"
 #include<avr/io.h>
+#include<util/delay.h>
 /**
  * @brief Functions
  * 
  */
 void pro_1(void){
-    DDRB|=(1<<PB0);// SET B0=1
+  DDRB|=(1<<PB0);// SET B0=1
   DDRD&=~(1<<PD2);// CLEAR BIT
   PORTD|=(1<<PD2);// SET BIT
   DDRD&=~(1<<PD1);// CLEAR BIT
@@ -34,56 +35,54 @@ int main(void){
     pro_1();
     while(1){
         if(sw1 && sw2){
-            led(LED_ON);//LED is ON
-            temp=ReadADC(0);
-            OCR1A=temp;
-            if(OCR1A>=0 && OCR1A<=200){
-                unsigned char d1[]="Temp:20\n";
+        led(LED_ON);//LED is ON
+        temp=ReadADC(0);
+        OCR1A=temp;
+        if(OCR1A>=0 && OCR1A<=200){
+        unsigned char d1[]="Temperature :20 degree Celsius\n";
         i=0;
         while(d1[i]!=0){
-           while(!(UCSR0A & (1<<UDRE0)));
-           UDR0=d1[i];
-           i++;
+        while(!(UCSR0A & (1<<UDRE0)));
+        UDR0=d1[i];
+        i++;
         }
-        delay_ms(LED_ON_TIME);
+        _delay_ms(200);
         }
         else if(OCR1A>=210 && OCR1A<=500){
-
-       unsigned char d1[]="Temp:25\n";
+        unsigned char d1[]="Temperature :25 degree Celsius\n";
         i=0;
         while(d1[i]!=0){
-           while(!(UCSR0A & (1<<UDRE0)));
-           UDR0=d1[i];
-           i++;
+        while(!(UCSR0A & (1<<UDRE0)));
+        UDR0=d1[i];
+        i++;
         }
-         delay_ms(LED_ON_TIME);
+        _delay_ms(200);
         }
         else if(OCR1A>=510 && OCR1A<=700){
-
-        unsigned char d1[]="Temp:29\n";
+        unsigned char d1[]="Temperature :29 degree Celsius\n";
         i=0;
         while(d1[i]!=0){
-           while(!(UCSR0A & (1<<UDRE0)));
-           UDR0=d1[i];
-           i++;
+        while(!(UCSR0A & (1<<UDRE0)));
+        UDR0=d1[i];
+        i++;
         }
-        delay_ms(LED_ON_TIME);
+        _delay_ms(200);
         }
         else if(OCR1A>=710 && OCR1A<=1024){
-       unsigned char d1[]="Temp:33\n";
+        unsigned char d1[]="Temperature :33 degree Celsius\n";
         i=0;
         while(d1[i]!=0){
-           while(!(UCSR0A & (1<<UDRE0)));
-           UDR0=d1[i];
-           i++;
+        while(!(UCSR0A & (1<<UDRE0)));
+        UDR0=d1[i];
+        i++;
         }
-            delay_ms(LED_ON_TIME);
+        _delay_ms(200);
         }
         }
     
     else{
         led(LED_OFF); //LED is OFF
-        delay_ms(LED_OFF_TIME);
+        _delay_ms(LED_OFF_TIME);
                 }
 }
 return 0;
